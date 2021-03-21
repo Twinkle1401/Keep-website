746981c8bea47f3620d4e3414506ad44d832b49d
Twinkle <twinklemakhija100@gmail.com>
2021-03-21 13:35:59 +0530

    my first commit reactwebsite

---
+++ src/Calc.jsx
@@ -0,0 +1,15 @@
+
+function Add(a,b){
+  return a+b;
+}
+function Sub(a,b){
+  return a-b;
+}
+function Div(a,b){
+  return a/b;
+}
+function Mul(a,b){
+  return a*b;
+}
+
+export {Add, Sub, Div, Mul};
